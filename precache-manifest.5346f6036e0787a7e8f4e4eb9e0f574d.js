a760d8bc59e21178d7e5eded44bf5ce5deac5289
Kniffen <mkniffen@bloomberg.com>
2019-09-15 20:13:37 -0400

    Updates

---
+++ precache-manifest.5346f6036e0787a7e8f4e4eb9e0f574d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d110c1ef56c21e1a9ede9ae3e12158b0",
+    "url": "/FallCandleGenerator/index.html"
+  },
+  {
+    "revision": "80c4f1b26043cb1f00dc",
+    "url": "/FallCandleGenerator/static/css/main.9540abb3.chunk.css"
+  },
+  {
+    "revision": "f498709293d9ee0a893c",
+    "url": "/FallCandleGenerator/static/js/2.51256eac.chunk.js"
+  },
+  {
+    "revision": "80c4f1b26043cb1f00dc",
+    "url": "/FallCandleGenerator/static/js/main.1a480cc5.chunk.js"
+  },
+  {
+    "revision": "e58dc8057220fdbb1754",
+    "url": "/FallCandleGenerator/static/js/runtime~main.6f11a1ef.js"
+  },
+  {
+    "revision": "d7bf468022fcb5ffbbaa247f6c02c5c8",
+    "url": "/FallCandleGenerator/static/media/background-2.d7bf4680.jpg"
+  },
+  {
+    "revision": "4169d27f9248b09d27bd8805b1ea5a3e",
+    "url": "/FallCandleGenerator/static/media/candle_transparent.4169d27f.png"
+  },
+  {
+    "revision": "b2f7252a760c11ddb79906cd161482e7",
+    "url": "/FallCandleGenerator/static/media/leaf1.b2f7252a.png"
+  }
+]);
